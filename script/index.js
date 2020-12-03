4de75f2f3bd69d62bfdceb40fe79ec44e572b674
Alexandr Raspertov <raspertovalexandr@yandex.ru>
2020-12-03 23:51:08 +0300

    feat: добавлено стандартные значения массива, лайк и удаление карт

--- script/index.js
+++ script/index.js
@@ -1,39 +1,135 @@
 // Объявление переменных
+const body = document.querySelector(".page");
+const authorName = document.querySelector(".profile__full-name");
+const authorDescription = document.querySelector(".profile__description");
 
-let popupOpened = document.querySelector('.popup'),
-    startFullName = document.querySelector('.profile__full-name'), 
-    startDescription = document.querySelector('.profile__description'),
-    fullName = document.querySelector('.popup__input[name="full-name"]'),
-    description = document.querySelector('.popup__input[name="description"]'),
-    formElement = document.querySelector('.popup__container'),
-    editButton = document.querySelector('.profile__button-edit'),
-    closeButton = document.querySelector('.popup__button-close');
+const popupOpened = document.querySelector(".popup");
+const popupOpeneda = document.querySelector(".popup").content;
+const popupForm = popupOpened.querySelector(".popup__form");
+const popupTitle = popupOpened.querySelector(".popup__title");
+const firstInput = popupOpened.querySelectorAll(".popup__input")[0];
+const secondInput = popupOpened.querySelectorAll(".popup__input")[1];
+const popupClosed = popupOpened.querySelector(".popup__button-close");
 
-// Функция появления popup и внесения данных пользователя в input
+// Функция появления popup
 function popupOpen() {
-  popupOpened.classList.add('popup_opened');
-
-  fullName.value = startFullName.textContent;
-  description.value = startDescription.textContent;
+  popupOpened.classList.add("popup_opened");
+}
+// Функция появления popup
+function popupClose() {
+  popupOpened.classList.remove("popup_opened");
 }
 
-// Функция закрытия popup
-function popupClose() {
-  popupOpened.classList.remove('popup_opened');
+popupClosed.addEventListener("click", popupClose);
+
+// Универсальная форма
+function bodyForm() {
+  popupOpen();
+
+  const formElement = popupOpeneda.cloneNode(true);
+
+  // Добавление html-кода в конец перед body
+  body.append(formElement);
 }
 
 // Сохранение данных
-function formSubmitHandler (evt) {
-    evt.preventDefault(); // Эта строчка отменяет стандартную отправку формы.
+function formSubmitHandler(evt) {
+  evt.preventDefault(); // Эта строчка отменяет стандартную отправку формы.
 
-    // Вставьте новые значения с помощью textContent
-    startFullName.textContent = fullName.value;
-    startDescription.textContent = description.value;
-    // Закрывает popup после нажатие на Сохранить
-    popupClose();
+  // Вставьте новые значения с помощью textContent
+  authorName.textContent = firstInput.value;
+  authorDescription.textContent = secondInput.value;
+
+  popupClose();
 }
 
-formElement.addEventListener('submit', formSubmitHandler); // Прикрепляем обработчик к форме: он будет следить за событием “submit” - «отправка»
+popupForm.addEventListener("submit", formSubmitHandler);
 
-editButton.addEventListener('click', popupOpen); 
-closeButton.addEventListener('click', popupClose); 
+const editButton = document.querySelector(".profile__button-edit");
+editButton.addEventListener("click", function () {
+  // Добавление значений атрибутам
+  popupTitle.textContent = "Редактировать профиль";
+  firstInput.placeholder = "Имя";
+  secondInput.placeholder = "О себе";
+  // Вставил в форму значения, которые уже на сайте
+  firstInput.value = authorName.textContent;
+  secondInput.value = authorDescription.textContent;
+
+  bodyForm();
+});
+
+
+const addButton = document.querySelector(".profile__button-add");
+addButton.addEventListener("click", function () {
+  // Добавление значений атрибутам
+  popupTitle.textContent = "Место";
+  firstInput.placeholder = "Название";
+  secondInput.placeholder = "Ссылка на картинку";
+  // Обнуление значений input
+  firstInput.value = '';
+  secondInput.value = '';
+  bodyForm();
+});
+
+// Добавляем карты на сайт
+const initialCards = [
+  {
+      name: 'Архыз',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
+  },
+  {
+      name: 'Челябинская область',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
+  },
+  {
+      name: 'Иваново',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
+  },
+  {
+      name: 'Камчатка',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
+  },
+  {
+      name: 'Холмогорский район',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
+  },
+  {
+      name: 'Байкал',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
+  }
+]; 
+
+const сardsContainerElement = document.querySelector('.elements__container');	
+const templateElement = document.querySelector('#card-template');
+
+function renderList() {
+  const сardsItems = initialCards.map(composeItem);
+  сardsContainerElement.append(...сardsItems);
+};
+
+function composeItem(item) {
+  const newCard = templateElement.content.cloneNode(true);
+  
+  const nameElement = newCard.querySelector('.element__location');
+  nameElement.textContent = item.name;
+  const linkElement = newCard.querySelector('.element__image');
+  linkElement.src = item.link;
+  
+  const likeButton = newCard.querySelector('.element__button-like');
+  likeButton.addEventListener('click', evt => {
+    likeButton.classList.toggle('element__button-like_active');
+  }); 
+
+  const deleteButton = newCard.querySelector('.element__button-delete');
+  deleteButton.addEventListener('click', removeItem);
+
+  return newCard;
+};
+
+function removeItem(event){
+  const targetElement = event.target;
+  const targetItem = targetElement.closest('.element');
+  targetItem.remove();
+};
+
+renderList();
